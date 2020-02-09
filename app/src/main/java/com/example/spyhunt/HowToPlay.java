58d6d388547eae8f00bb828a3fcd4fe2c6ad4d35
AidinSh <aidin_shahmoradi@yahoo.com>
2020-02-09 22:58:58 +0330

    Fix How To Play and Timer

--- app/src/main/java/com/example/spyhunt/HowToPlay.java
+++ app/src/main/java/com/example/spyhunt/HowToPlay.java
@@ -2,7 +2,9 @@ package com.example.spyhunt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class HowToPlay extends AppCompatActivity {
@@ -15,5 +17,13 @@ public class HowToPlay extends AppCompatActivity {
         setContentView(R.layout.activity_how_to_play);
 
         btnBack = findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HowToPlay.this, com.example.spyhunt.MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/spyhunt/Timer.java
+++ app/src/main/java/com/example/spyhunt/Timer.java
@@ -3,12 +3,29 @@ package com.example.spyhunt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
 public class Timer extends AppCompatActivity {
+
+    TextView tvTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
+
+        tvTimer = findViewById(R.id.tvTimer);
+
+       /* final CountDownTimer timer = new CountDownTimer(30000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                tvTimer.setText("seconds remaining: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                tvTimer.setText("زمان به پایان رسید!");
+            }
+        }.start();*/
     }
 }
